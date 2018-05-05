d784b63eabb354500eee6016e6c3daa9fd7e3a61
Chonlasith Jucksriporn <chonlasith@gmail.com>
2018-05-05 17:09:48 +0700

    Add tests

--- src/app/pipes/operator.pipe.spec.ts
+++ src/app/pipes/operator.pipe.spec.ts
@@ -1,10 +1,28 @@
 import { OperatorPipe } from './operator.pipe';
 import { DomSanitizer } from '@angular/platform-browser';
-import { inject } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 
 describe('OperatorPipe', () => {
   it('create an instance', inject([DomSanitizer], (domSanitizer: DomSanitizer) => {
     const pipe = new OperatorPipe(domSanitizer);
     expect(pipe).toBeTruthy();
   }));
+
+  it('should return safe html if defined', () => {
+    var domSanitizer = jasmine.createSpyObj('domSanitizer', ['bypassSecurityTrustHtml']);
+    const pipe = new OperatorPipe(domSanitizer);
+
+    var result = pipe.transform('*');
+
+    expect(domSanitizer.bypassSecurityTrustHtml).toHaveBeenCalledWith('&times;');
+  });
+
+  it('should return blank if not defined', () => {
+    var domSanitizer = jasmine.createSpyObj('domSanitizer', ['bypassSecurityTrustHtml']);
+    const pipe = new OperatorPipe(domSanitizer);
+
+    var result = pipe.transform('<');
+
+    expect(domSanitizer.bypassSecurityTrustHtml).toHaveBeenCalledWith('&nbsp;');
+  });
 });
